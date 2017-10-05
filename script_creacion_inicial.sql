3919a7cb256ff569b619a605610e63cf59e5821e
lucas-avila <lucasrotelavila@gmail.com>
2017-10-05 17:12:23 -0300

    Fixing data types.

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -58,53 +58,53 @@ CREATE TABLE GDD_FORK.Entry (
 GO
 
 CREATE TABLE GDD_FORK.Company (
-	com_cuit int NOT NULL,
+	com_cuit nvarchar(50) NOT NULL,
 	com_ent_id int NOT NULL,
-	com_name varchar(100) NOT NULL,
-	com_address varchar(150) NOT NULL,
+	com_name nvarchar(255) NOT NULL,
+	com_address nvarchar(255) NOT NULL,
 	CONSTRAINT Company_PK PRIMARY KEY (com_cuit),
 	FOREIGN KEY (com_ent_id) REFERENCES GDD_FORK.Entry(ent_id))
 GO
 
 CREATE TABLE GDD_FORK.Invoice (
-	inv_nro int identity NOT NULL,
-	inv_date date NOT NULL,
+	inv_nro numeric(18, 0) NOT NULL,
+	inv_date datetime NOT NULL,
 	inv_amount numeric NOT NULL,
-	inv_total numeric NOT NULL,
+	inv_total numeric(18, 2) NOT NULL,
 	inv_fee_percentage numeric NOT NULL,
 	inv_subtotal numeric NOT NULL,
-	inv_company_cuit int NOT NULL, 
+	inv_company_cuit nvarchar(255) NOT NULL, 
 	CONSTRAINT Invoice_PK PRIMARY KEY (inv_nro),
 	FOREIGN KEY (inv_company_cuit) REFERENCES GDD_FORK.Company(com_cuit))
 GO
 
 CREATE TABLE GDD_FORK.Client (
 	cli_id int identity NOT NULL,
-	clid_dni numeric(8, 0) NOT NULL,
-	cli_name varchar(100) NOT NULL,
-	cli_last_name varchar(100) NOT NULL,
-	cli_date_birth date NOT NULL,
-	cli_email nvarchar (100),
-	cli_address varchar(150) NOT NULL,
-	cli_postal_code numeric(18, 0) NOT NULL,
+	clid_dni numeric(18, 0) NOT NULL,
+	cli_name nvarchar(255) NOT NULL,
+	cli_last_name nvarchar(255) NOT NULL,
+	cli_date_birth datetime NOT NULL,
+	cli_email nvarchar(255),
+	cli_address nvarchar(255) NOT NULL,
+	cli_postal_code nvarchar(255) NOT NULL,
 	CONSTRAINT Client_PK PRIMARY KEY (cli_id, cli_dni))
 GO
 
 CREATE TABLE GDD_FORK.Payment_Method (
 	paym_id int identity NOT NULL,
-	paym_description varchar(150) NOT NULL,
+	paym_description nvarchar(255) NOT NULL,
 	CONSTRAINT Payment_Method_PK PRIMARY KEY (paym_id))
 GO
 
 CREATE TABLE GDD_FORK.Bill (
-	bill_number int identity NOT NULL,
-	bill_cli_dni numeric(8, 0) NOT NULL,
-	bill_com_cuit int NOT NULL, /*bill_com_dni??*/
-	bill_ref_id int NOT NULL,
-	bill_inv_nro int NOT NULL,
-	bill_date date NOT NULL, 	/*Bill ggggggates.*/
-	bill_total numeric NOT NULL,
-	bill_expiration date NOT NULL,
+	bill_number numeric(18, 0) NOT NULL,
+	bill_cli_dni numeric(18, 0) NOT NULL,
+	bill_com_cuit nvarchar(255) NOT NULL, /*bill_com_dni??*/
+	bill_ref_id numeric(18, 0) NOT NULL,
+	bill_inv_nro numeric(18, 0) NOT NULL,
+	bill_date datetime NOT NULL, 	/*Bill ggggggates.*/
+	bill_total numeric(18, 2) NOT NULL,
+	bill_expiration datetime NOT NULL,
 	CONSTRAINT Bill_PK PRIMARY KEY (bill_number),
 	FOREIGN KEY (bill_cli_dni) REFERENCES GDD_FORK.Client(cli_dni),
 	FOREIGN KEY (bill_com_cuit) REFERENCES GDD_FORK.Company(com_cuit),
@@ -113,13 +113,13 @@ CREATE TABLE GDD_FORK.Bill (
 GO
 
 CREATE TABLE GDD_FORK.Payment (
-	pay_number int identity NOT NULL,
-	pay_bill_number int NOT NULL,
+	pay_number numeric(18, 0) NOT NULL,
+	pay_bill_number numeric(18, 0) NOT NULL,
 	pay_branch_name nvarchar(50) NOT NULL,
 	pay_paym_id int NOT NULL,
-	pay_date date NOT NULL,
-	pay_item_number int NOT NULL,
-	pay_total numeric NOT NULL,
+	pay_date datetime NOT NULL,
+	pay_item_number numeric(18, 0) NOT NULL,
+	pay_total numeric(18, 2) NOT NULL,
 	CONSTRAINT Payment_PK PRIMARY KEY (pay_number),
 	FOREIGN KEY (pay_bill_number) REFERENCES GDD_FORK.Bill(bill_number),
 	FOREIGN KEY (pay_branch_name) REFERENCES GDD_FORK.Branch(branch_name),
