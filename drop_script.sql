d1da58284d3fd2f5b88346970f2b5cdc2703964c
lucas-avila <lucasrotelavila@gmail.com>
2017-10-23 00:07:54 -0300

    [Add] Drop script

---
+++ drop_script.sql
@@ -0,0 +1,137 @@
+DROP TABLE GDD_FORK.Role_user
+GO
+
+DROP TABLE GDD_FORK.Role_Funcionality
+GO
+
+DROP TABLE GDD_FORK.Role
+GO
+
+DROP TABLE GDD_FORK.Payment
+GO
+
+DROP TABLE GDD_FORK.Payment_Method
+GO
+
+DROP TABLE GDD_FORK.Users
+GO
+
+DROP TABLE GDD_FORK.Funcionality
+GO
+
+DROP TABLE GDD_FORK.Branch_user
+GO
+
+DROP TABLE GDD_FORK.Branch
+GO
+
+DROP TABLE GDD_FORK.Item
+GO
+
+DROP TABLE GDD_FORK.Bill_Refund
+GO
+
+DROP TABLE GDD_FORK.BillRefund
+GO
+
+DROP TABLE GDD_FORK.Bill
+GO
+
+DROP TABLE GDD_FORK.Invoice
+GO
+
+DROP TABLE GDD_FORK.Users
+GO
+
+DROP TABLE GDD_FORK.Company
+GO
+
+DROP TABLE GDD_FORK.Entry
+GO
+
+DROP TABLE GDD_FORK.Client
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_search_branchs] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_user] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_update_user_attempts] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_role] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_roles] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_all_roles] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_add_role] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_update_role] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_disable_role] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_branchs] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_functionalities] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_functionality] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_role_functionalities] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_remove_role_functionality] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_remove_role_from_users] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_link_functionality_role] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_search_branchs] 
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_change_active_branch]
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_check_postal_code_branch]
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_insert_update_branch]
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_all_entries]
+GO
+
+DROP PROCEDURE [GDD_FORK].[sp_get_all_companies]
+
+DROP PROCEDURE [GDD_FORK].[sp_create_client]
+
+DROP PROCEDURE [GDD_FORK].[sp_update_client]
+
+DROP PROCEDURE [GDD_FORK].[sp_delete_client]
+
+DROP PROCEDURE [GDD_FORK].[sp_select_client]
+
+DROP PROCEDURE [GDD_FORK].[sp_search_companies]
+
+DROP PROCEDURE [GDD_FORK].[sp_change_active_company]
+
+DROP PROCEDURE [GDD_FORK].[sp_insert_update_company]
+
+DROP PROCEDURE [GDD_FORK].[sp_check_cuit]
+
+DROP PROCEDURE [GDD_FORK].[sp_comany_can_be_disable]
+
